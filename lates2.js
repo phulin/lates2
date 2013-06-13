7e63dcbbddcfd173af06eb5f7eee590c086e453b
Patrick Hulin <patrick.hulin@gmail.com>
2013-06-13 11:12:10 -0400

    Initial commit

---
+++ lates2.js
@@ -0,0 +1,58 @@
+Lates = new Meteor.Collection('lates');
+
+months = [ "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December" ]
+days = [ "Sunday", "Monday", "Tuesday", "Wednesday",
+      "Thursday", "Friday", "Saturday" ];
+
+if (Meteor.isClient) {
+  Template.late_list.has_lates = function() {
+    return Lates.findOne({
+      'date': new Date().toDateString(),
+    }) ? true : false;
+  };
+
+  Template.late_list.todays_lates = function() {
+    return Lates.find({
+      'date': new Date().toDateString(),
+    });
+  };
+  
+  Template.late_list.today = function() {
+    var now = new Date();
+    return days[now.getDay()] + ', ' + months[now.getMonth()] + ' ' + now.getDate();
+  };
+
+  Template.late.rendered = function() {
+    var item = $(this.find('.late-item'));
+    var id = item.attr('id');
+    if (!Session.get(id)) {
+      item.hide();
+      Meteor.defer(function() { item.slideDown(); });
+      Session.set(id, true);
+    }
+    
+  };
+
+  Template.late_list.events = {
+    'click #request-submit': function(e) {
+      e.preventDefault();
+      Lates.insert({
+        'name': $('#name').val(),
+        'refrigerated': $('#refrigerated').val(),
+        'date': new Date().toDateString(),
+      });
+    },
+
+    'click .late-cancel': function(e) {
+      e.preventDefault();
+      var late_item = $(e.target).parents('.late-item');
+      var id = late_item.attr('id');
+      late_item.slideUp(400, function() {
+        Lates.remove({
+          '_id': id,
+        });
+      });
+    }
+  };
+}
